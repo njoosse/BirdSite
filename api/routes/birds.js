8e133d57df21880019ffead13a12fae3e179bfb5
Nathan <njoosse@hotmail.com>
2022-12-03 22:51:22 -0500

    API reads points from Postgres DB and formats into GeoJSON

--- api/routes/birds.js
+++ api/routes/birds.js
@@ -1,8 +1,45 @@
+require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-  res.send("got bird list");
+const pgp = require("pg-promise")(/* options */);
+
+const db = pgp(
+  "postgres://" +
+    process.env.pgUsername +
+    ":" +
+    process.env.pgPassword +
+    "@" +
+    process.env.pgServer +
+    ":" +
+    process.env.pgPort +
+    "/" +
+    process.env.pgDatabase
+);
+
+router.get("/", function (req, res, next) {
+  db.any('SELECT latitude, longitude, name FROM "birdApp"."birdPictures"')
+    .then((data) => {
+      console.log(typeof data);
+      let geoJson = { features: [] };
+      data.forEach((row) => {
+        geoJson.features.push({
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [row.longitude, row.latitude],
+          },
+          properties: {
+            name: row.name,
+          },
+        });
+      });
+      res.send(geoJson);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(error);
+    });
 });
 
 module.exports = router;
