0f17d2c792c1310b1e3a8f543b8222db228c86f0
Nathan <njoosse@hotmail.com>
2022-12-07 18:22:11 -0500

    formatting

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,11 +5,14 @@ import React from 'react';
 import { LeafletMap } from './components/map/LeafletMap';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <LeafletMap startPosition={[43.5385, -80.2415]} startZoom={17} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <LeafletMap
+                    startPosition={[43.5385, -80.2415]}
+                    startZoom={17}
+                />
+            </div>
+        );
+    }
 }
