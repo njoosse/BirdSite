10f29ad6546c25939e28bccb5a59e0ab88bdbab1
Nathan Joosse <njoosse@hotmail.com>
2022-12-11 21:59:32 -0500

    zoom to full extent of points when loaded

--- ui/src/components/map/LeafletMap.js
+++ ui/src/components/map/LeafletMap.js
@@ -12,13 +12,37 @@ export class LeafletMap extends React.Component {
             position: props.startPosition,
             zoom: props.startZoom,
             birdData: { features: [] },
+            map: React.createRef(),
         };
     }
 
     getBirdTable() {
         fetch('http://localhost:9000/birds')
             .then((res) => res.json())
-            .then((res) => this.setState({ birdData: res }));
+            .then((res) => this.setState({ birdData: res }))
+            .then((res) => this.getPointExtent());
+    }
+
+    getPointExtent() {
+        console.log(this.state.birdData);
+        // initialize the values at the opposite so we get the minimum bounding box
+        let minX = 180;
+        let minY = 90;
+        let maxX = -180;
+        let maxY = -90;
+
+        this.state.birdData.features.forEach((feature) => {
+            minX = Math.min(minX, feature.geometry.coordinates[0]);
+            minY = Math.min(minY, feature.geometry.coordinates[1]);
+            maxX = Math.max(maxX, feature.geometry.coordinates[0]);
+            maxY = Math.max(maxY, feature.geometry.coordinates[1]);
+        });
+
+        let bounds = [
+            [minY, minX],
+            [maxY, maxX],
+        ];
+        this.state.map.current.fitBounds(bounds);
     }
 
     componentDidMount() {
@@ -28,9 +52,11 @@ export class LeafletMap extends React.Component {
     render() {
         return (
             <MapContainer
+                ref={this.state.map}
                 className="MapContainer"
                 center={this.state.position}
                 zoom={this.state.zoom}
+                bounds={this.state.bounds}
                 scrollWheelZoom={true}
             >
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
